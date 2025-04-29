a7a28c1781cdc2c16accd0042754d144db9b266d
NotJesus172 <157871959+NotJesus172@users.noreply.github.com>
2025-04-29 00:38:05 -0400

    fedora application add

--- fedora.sh
+++ fedora.sh
@@ -3,7 +3,7 @@
 #	Purge any docker packages from fedora install:
 sudo dnf remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-selinux docker-engine-selinux docker-engine
 #	Add repos and install basic utilities:
-sudo dnf -y install dnf-plugins-core fastfetch
+sudo dnf -y install dnf-plugins-core fastfetch git curl nano
 sudo dnf-3 config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
 #	Install the Docker packages:
 sudo dnf -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
